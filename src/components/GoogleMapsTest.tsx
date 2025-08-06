1fd02134010dc844adf51830e5447fc3968254d2
Eapechan <haroneapen@gmial.com>
2025-08-06 14:11:51 +0530

    chore: add placeholder GoogleMapsTest component

---
+++ src/components/GoogleMapsTest.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GoogleMapsTest: React.FC = () => {
+  return (
+    <div style={{ padding: 20, background: '#eee', borderRadius: 8 }}>
+      GoogleMapsTest placeholder component
+    </div>
+  );
+};
+
+export { GoogleMapsTest };
